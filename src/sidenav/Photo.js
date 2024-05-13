c9fec00d90ab94f33f36a28e00c7a5fc405601cd
Pedro Lemos <lemos.pj@gmail.com>
2024-05-13 12:24:38 -0400

    Add alt prop to picture

--- src/sidenav/Photo.js
+++ src/sidenav/Photo.js
@@ -3,7 +3,7 @@ import myimg from "../assets/mypic.jpg"
 function Photo() {
     return (
         <div className="photo">
-            <img src={myimg} className="mypicture" title="This is me" />
+            <img src={myimg} className="mypicture" alt="My picture" title="This is me" />
             <div className="name">Pedro Lemos</div>
         </div>
     )
