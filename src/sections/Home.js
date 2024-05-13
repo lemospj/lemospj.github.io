23c9dd499590e5cd00d4c1be513f798364f367cb
Pedro Lemos <lemos.pj@gmail.com>
2024-05-13 13:53:24 -0400

    Add hyperlinks

--- src/sections/Home.js
+++ src/sections/Home.js
@@ -4,9 +4,9 @@ function Home() {
             <h1>Welcome!</h1>
 
             <p>
-                I am a mathematician and software engineer based in Philadelphia. I completed my PhD at the University of Warwick in 2017 under the supervision of Samir Siksek. Since then,
+                I am a mathematician and software engineer based in Philadelphia. I completed my PhD at the University of Warwick in 2017 under the supervision of <a href="https://homepages.warwick.ac.uk/staff/S.Siksek/" target="_blank" rel="noopener noreferrer">Samir Siksek</a>. Since then,
                 I held two postdoctoral positions &mdash; one at the Max Planck Institute for Mathematics in Bonn, Germany, and the other one at University
-                College London &mdash; and I worked as a full stack software engineer at Enable for nearly two years. I moved to Philadelphia in October 2023
+                College London &mdash; and I worked as a full stack software engineer at <a href="https://enable.com/" target="_blank" rel="noopener noreferrer">Enable</a> for nearly two years. I moved to Philadelphia in October 2023
                 and taught Calculus I during the Fall 2023 semester at Temple University.
             </p>
             <p>
